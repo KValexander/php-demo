6bb6b1e39e49313dcb615bba14a99d852434e80b
KValexander <ewoase@gmail.com>
2022-03-03 10:42:57 +0300

    delete upd

--- php-demo-2022/step2 PHP/controllers/delete_product.php
+++ php-demo-2022/step2 PHP/controllers/delete_product.php
@@ -1,5 +1,5 @@
 <?php
 	include "check_admin.php";
 	include "../connect.php";
-	$connect->query("DELETE `products`, `orders` FROM `products` RIGHT JOIN `orders` USING(`product_id`) WHERE `product_id`=".$_GET["id"]);
+	$connect->query("DELETE `products` FROM `products` WHERE `product_id`=".$_GET["id"]);
 	return header("Location:../admin?message=Товар удалён");
